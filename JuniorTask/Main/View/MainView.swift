907f6183444676d2aba60916aa74129ac69f8ca4
MnStan <mmaks97@gmail.com>
2024-10-30 17:37:54 +0100

    Created folders strucure Created .plist file for API key and added it to .gitignore

---
+++ JuniorTask/Main/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  JuniorTask
+//
+//  Created by Maksymilian Stan on 30/10/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ JuniorTask/Managers/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  JuniorTask
+//
+//  Created by Maksymilian Stan on 30/10/2024.
+//
+
+import Foundation
+
+class NetworkManager {
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "ticketmaster-info", ofType: "plist") else {
+                fatalError("Couldn't find file 'ticketmaster-info.plist'.")
+            }
+            
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "API_KEY") as? String else {
+                fatalError("Couldn't find key 'API_KEY in 'tickemaster-info.plist'.")
+            }
+            
+            return value
+        }
+    }
+}
