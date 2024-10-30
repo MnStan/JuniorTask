1735265a159b93095692b6788bb8dc0d3b9120d3
MnStan <mmaks97@gmail.com>
2024-10-30 14:24:10 +0100

    Initial Commit

---
+++ JuniorTask/JuniorTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuniorTaskApp.swift
+//  JuniorTask
+//
+//  Created by Maksymilian Stan on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JuniorTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
